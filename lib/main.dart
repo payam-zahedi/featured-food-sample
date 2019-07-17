ea886e9c54e8d0b4ba2b7f1e3c06f182f862d1d5
payam <pzahedi1995@yahoo.com>
2019-07-17 16:19:35 +0430

    icon created

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Featured Food",
